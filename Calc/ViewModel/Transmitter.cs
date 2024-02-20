06f6fe95f6efc27f1249d986ea5b4cc8ad8db7fd
Besh- <Besh-@HOMEPC>
2024-02-20 17:48:00 +0600

    Add project files.

---
+++ Calc/ViewModel/Transmitter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calc.ViewModel
+{
+    public class Transmitter : INotifyPropertyChanged
+    {
+        public string СontentOnButton { get; set; }
+        public double Number { get; set; }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public void ButtonTextOnNumber(string text)
+        {
+            if (double.TryParse(text, out double result))
+            {
+                Number = result;
+            }
+            else
+            {
+                MessageBox.Show("IsNotANumber"); //это логика, возможно переместить позже. 
+            }
+        }
+    }
+}
